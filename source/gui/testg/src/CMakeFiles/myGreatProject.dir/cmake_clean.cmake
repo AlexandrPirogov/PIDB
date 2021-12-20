3b36d3fcd6e49d3fb69a50bc7cd7c78ca36ef747
AlexandrPirogov <cat_shot12@mail.ru>
2021-12-19 17:13:38 -0900

    first gui

---
+++ source/gui/testg/src/CMakeFiles/myGreatProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myGreatProject.dir/MyApp.cpp.o"
+  "CMakeFiles/myGreatProject.dir/simple.cpp.o"
+  "myGreatProject"
+  "myGreatProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myGreatProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source/gui/testg/src/CMakeFiles/projecmyGreatProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projecmyGreatProject.dir/Forty.cpp.o"
+  "CMakeFiles/projecmyGreatProject.dir/simple.cpp.o"
+  "projecmyGreatProject"
+  "projecmyGreatProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projecmyGreatProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
